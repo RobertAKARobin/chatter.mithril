b4e2bfb13ab8a3f96798f8090c45dfdd383f648a
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-13 00:30:59 -0500

    Adds posts to convos

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const httpServer = express()
 const baseServer = http.createServer(httpServer)
 const socketServer = socketio(baseServer)
 const db = {
-	convoList: {}
+	convoList: {},
+	convo: {}
 }
 
 let convoID = 0
@@ -17,8 +18,21 @@ function addConvo(title){
 		id: convoID,
 		title: title
 	}
+	const convo = {
+		id: convoID,
+		posts: []
+	}
 	db.convoList[convoID] = convoListItem
+	db.convo[convoID] = convo
 	return convoListItem
+}
+
+let postID = 0
+function addPost(convoID, text){
+	postID += 1
+	db.convo[convoID].posts[postID] = {
+		text
+	}
 }
 
 new function seed(){
@@ -49,7 +63,14 @@ httpServer
 		res.json({success: true})
 	})
 	.get('/convo/:id', (req, res) => {
+		const id = req.params.id
+		const convo = db.convoList[id]
+		convo.posts = Object.values(db.convo[id].posts)
 		res.json({
-			convo: db.convoList[req.params.id]
+			convo
 		})
+	})
+	.post('/convo/:id', (req, res) => {
+		addPost(req.params.id, req.body.post.text)
+		res.json({success: true})
 	})
--- public/js/main.js
+++ public/js/main.js
@@ -69,6 +69,28 @@ var ConvoList = (function(){
 
 var Convo = (function(){
 	var convo = {};
+	convo.post = function(post){
+		return m.request({
+			method: 'POST',
+			url: './convo/' + convo.data.id,
+			data: post
+		});
+	}
+
+	var events = {};
+	events.post = function(event){
+		var input = event.target;
+		var isReturn = (event.keyCode == 13);
+		event.redraw = false;
+		if(isReturn && input.value){
+			convo.post({
+				post: {
+					text: input.value
+				}
+			});
+			input.value = '';
+		}
+	}
 
 	var component = {};
 	component.oninit = function(vnode){
@@ -78,7 +100,17 @@ var Convo = (function(){
 	}
 	component.view = function(){
 		if(convo.data){
-			return m('h1', convo.data.id + ': ' + convo.data.title);
+			return [
+				m('h1', convo.data.id + ': ' + convo.data.title),
+				m('ul', [
+					m('li', [
+						m('input', {onkeyup: events.post})
+					]),
+					convo.data.posts.map(function(post){
+						return m('li', post.text);
+					})
+				])
+			];
 		}else{
 			return m('h1', 'Loading...');
 		}
