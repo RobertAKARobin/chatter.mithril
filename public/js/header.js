40403994d92dbfb5f167235b9e7c30639ebdd72d
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-18 17:49:06 -0500

    Moved User back to Header because it'll never be outside it

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,64 @@
 'use strict';
 
 var Header = (function(){
+	var user = {};
+	user.isSignedIn = false;
+
+	var newUser = {};
+	newUser.construct = function(){
+		newUser.data = {
+			name: m.stream(''),
+			password: m.stream('')
+		}
+	}
+	newUser.save = function(){
+		return m.request({
+			method: 'POST',
+			url: './users',
+			data: {
+				user: newUser.data
+			}
+		});
+	}
+
+	var events = {};
+	events.signUp = function(event){
+		event.redraw = false;
+		newUser.save().then(function(response){
+			if(response.success){
+				user.data = response.user;
+				user.isSignedIn = true;
+				m.redraw();
+			}
+		});
+	}
+
+	var views = {};
+	views.user = function(){
+		if(user.isSignedIn == true){
+			return views.isSignedIn();
+		}else{
+			return views.signUp();
+		}
+	}
+	views.signUp = function(){
+		return [
+			m('input', m._boundInput(newUser.data.name, {
+				placeholder: 'Name'
+			})),
+			m('input', m._boundInput(newUser.data.password, {
+				placeholder: 'Password',
+				type: 'password'
+			})),
+			m('button', {
+				onclick: events.signUp
+			}, 'Sign up')
+		];
+	}
+	views.isSignedIn = function(){
+		return m('p', 'Welcome, ' + user.data.name + '!');
+	}
+
 	var component = {};
 	component.view = function(){
 		return [
@@ -10,8 +68,11 @@ var Header = (function(){
 					oncreate: m.route.link
 				}, 'Home')
 			]),
-			User.view()
+			views.user()
 		];
+	}
+	component.oninit = function(){
+		newUser.construct();
 	}
 	return component;
 })();
