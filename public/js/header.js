33b122e4cf4fc1e63e2679b904dfbd98517b489c
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-18 16:51:18 -0500

    Moved User to own file

--- public/js/header.js
+++ public/js/header.js
@@ -1,60 +1,6 @@
 'use strict';
 
 var Header = (function(){
-	var user = {};
-	user.data = {};
-	user.input = {
-		name: m.stream(''),
-		password: m.stream('')
-	}
-	user.isSignedIn = false;
-	user.signUp = function(input){
-		return m.request({
-			method: 'POST',
-			url: './users',
-			data: input
-		}).then(function(response){
-			user.isSignedIn = true;
-			user.data = response.user;
-		});
-	}
-
-	user.events = {};
-	user.events.signUp = function(event){
-		event.redraw = false;
-		user.signUp({
-			user: user.input
-		});
-	}
-
-	user.view = function(){
-		if(user.isSignedIn == true){
-			return user.views.isSignedIn();
-		}else{
-			return user.views.signUp();
-		}
-	}
-	user.views = {};
-	user.views.signUp = function(){
-		return [
-			m('input', {
-				placeholder: 'Name',
-				oninput: m.value(user.input.name)
-			}),
-			m('input', {
-				placeholder: 'Password',
-				type: 'password',
-				oninput: m.value(user.input.password)
-			}),
-			m('button', {
-				onclick: user.events.signUp
-			}, 'Sign up')
-		];
-	}
-	user.views.isSignedIn = function(){
-		return m('p', 'Welcome, ' + user.data.name + '!');
-	}
-
 	var component = {};
 	component.view = function(){
 		return [
@@ -64,7 +10,7 @@ var Header = (function(){
 					oncreate: m.route.link
 				}, 'Home')
 			]),
-			user.view()
+			User.view()
 		];
 	}
 	return component;
---
+++ public/js/user.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var User = (function(){
+	var user = {};
+	user.data = {};
+	user.input = {
+		name: m.stream(''),
+		password: m.stream('')
+	}
+	user.isSignedIn = false;
+	user.signUp = function(input){
+		return m.request({
+			method: 'POST',
+			url: './users',
+			data: input
+		}).then(function(response){
+			user.isSignedIn = true;
+			user.data = response.user;
+		});
+	}
+
+	user.events = {};
+	user.events.signUp = function(event){
+		event.redraw = false;
+		user.signUp({
+			user: user.input
+		});
+	}
+
+	user.view = function(){
+		if(user.isSignedIn == true){
+			return user.views.isSignedIn();
+		}else{
+			return user.views.signUp();
+		}
+	}
+	user.views = {};
+	user.views.signUp = function(){
+		return [
+			m('input', {
+				placeholder: 'Name',
+				oninput: m.value(user.input.name)
+			}),
+			m('input', {
+				placeholder: 'Password',
+				type: 'password',
+				oninput: m.value(user.input.password)
+			}),
+			m('button', {
+				onclick: user.events.signUp
+			}, 'Sign up')
+		];
+	}
+	user.views.isSignedIn = function(){
+		return m('p', 'Welcome, ' + user.data.name + '!');
+	}
+
+	return user;
+})();
