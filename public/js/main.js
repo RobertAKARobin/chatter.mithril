e649507ae6b8830c665b92a1768f5c3399519233
RobertAKARobin <robertgfthomas@gmail.com>
2017-03-18 16:29:54 -0500

    Added withAttrStatic util

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+m.withAttrStatic = function(attribute, callback){
+	var eventHandler = m.withAttr(attribute, callback);
+	return function(event){
+		event.redraw = false;
+		eventHandler(event);
+	}
+}
+
 document.addEventListener('DOMContentLoaded', function(){
 	m.mount(document.getElementById('header'), Header);
 	m.route(document.getElementById('main'), '/', {
